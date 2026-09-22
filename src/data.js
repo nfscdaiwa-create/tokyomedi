f61caccc672a30d0ce5343d5b4f7c3c35d5e0c0c
nfscdaiwa-create <nfsc.daiwa@gmail.com>
2026-09-22 23:12:12 +0800

    Update TOKYO MEDI contact email

--- src/data.js
+++ src/data.js
@@ -1,5 +1,5 @@
 export const SITE = 'https://tokyomedi.com';
-export const EMAIL = 'business@tokyomedi.com';
+export const EMAIL = 'beibei7jp1978@yahoo.co.jp';
 export const VERSION = '2026.09.22-rebuild';
 export const LOCALES = ['zh-hans','en','ja'];
 
